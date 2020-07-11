7e768f2416af6cc6797fe96e2b0eaa951b685486
Michael Poore <mpoore@mpoore-a01.vmware.com>
2020-07-11 15:23:34 +0100

    Changed formatting

--- scripts/centos/95-motd.sh
+++ scripts/centos/95-motd.sh
@@ -16,8 +16,8 @@ cat << ISSUE > /etc/issue
   {__ {__   {__    {__     {__  {__
    {_{__    {__  {__       {__  {__
     {__    {____{________ {___  {__
-            v12n - $NAME - REPLACEWITHBUILDVERSION
-            $DOCS
+        v12n - $NAME - REPLACEWITHBUILDVERSION
+        $DOCS
 
 ISSUE
 
