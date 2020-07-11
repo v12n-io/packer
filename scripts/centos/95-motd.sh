96cc217cb7ef3bd3a76c748f280ce337e722bae9
Michael Poore <mpoore@mpoore-a01.vmware.com>
2020-07-11 10:20:34 +0100

    Script changes

---
+++ scripts/centos/95-motd.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Prepare Centos OS template for vSphere cloning
+# @author Michael Poore
+# @website https://blog.v12n.io
+
+# Build date used for motd
+BUILDDATE="REPLACEWITHBUILDVERSION"
+NAME="CentOS 8"
+DOCS="https://github.com/v12n-io/packer"
+
+# Create Issue
+cat << ISSUE > /etc/issue
+
+           {__   {__ {_            
+{__     {__ {__ {_     {__{__ {__  
+ {__   {__  {__      {__   {__  {__
+  {__ {__   {__    {__     {__  {__
+   {_{__    {__  {__       {__  {__
+    {__    {____{________ {___  {__
+            v12n ($NAME $BUILDDATE)
+            $DOCS
+
+ISSUE
+
+# MOTD symlinks
+ln -sf /etc/issue /etc/issue.net
