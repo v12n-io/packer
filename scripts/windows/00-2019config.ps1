871242d8468f15ba01d5bb6976aaa1cf4fa2cfa2
Michael Poore <mpoore@mpoore-a01.vmware.com>
2020-07-12 16:38:06 +0100

    Windows 2019

---
+++ scripts/windows/00-2019config.ps1
@@ -0,0 +1,22 @@
+# Basic configuration of vanilla Windows Server installation to progress Packer.io builds
+# @author Michael Poore
+# @website https://blog.v12n.io
+# @source https://github.com/virtualhobbit
+$ErrorActionPreference = "Stop"
+
+# Switch network connection to private mode
+# Required for WinRM firewall rules
+$profile = Get-NetConnectionProfile
+Set-NetConnectionProfile -Name $profile.Name -NetworkCategory Private
+
+# Enable Network Discovery
+netsh advfirewall firewall set privateprofile rule group="Network Discovery" new enable=Yes
+
+# Enable WinRM service
+winrm quickconfig -quiet
+winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+winrm set winrm/config/service/auth '@{Basic="true"}'
+
+# Reset auto logon count
+# https://docs.microsoft.com/en-us/windows-hardware/customize/desktop/unattend/microsoft-windows-shell-setup-autologon-logoncount#logoncount-known-issue
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name AutoLogonCount -Value 0
