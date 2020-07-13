8a7e0d5bce5fb3d2ba855be62a8fe00fb35e4071
Michael Poore <mpoore@mpoore-a01.vmware.com>
2020-07-13 11:13:45 +0100

    First cut of Ubuntu 1804

---
+++ scripts/ubuntu/00-update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Prepare Ubuntu template for vSphere cloning with updates
+# @author Michael Poore
+# @website https://blog.v12n.io
+
+# Update existing packages
+sudo apt update -y
+sudo apt upgrade -y
