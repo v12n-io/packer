13efc8b7a5b895edf0c78fe54134fda31929727e
mpoore <2269147+mpoore@users.noreply.github.com>
2021-02-25 13:05:50 +0000

    Disable firewall and set network profile

--- script/windows/01-initialise.ps1
+++ script/windows/01-initialise.ps1
@@ -7,10 +7,14 @@ $ErrorActionPreference = "Stop"
 # Switch network connection to private mode
 # Required for WinRM firewall rules
 $profile = Get-NetConnectionProfile
+While ($profile.Name -eq "Identifying..."){
+    Start-Sleep -Seconds 10
+    $profile = Get-NetConnectionProfile
+}
 Set-NetConnectionProfile -Name $profile.Name -NetworkCategory Private
 
-# Enable Network Discovery
-netsh advfirewall firewall set privateprofile rule group="Network Discovery" new enable=Yes
+# Drop the firewall while building and re-enable as a standalone provisioner in the Packer file if needs be
+netsh Advfirewall set allprofiles state off
 
 # Enable WinRM service
 winrm quickconfig -quiet
