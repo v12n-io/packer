0caa81efe29678532a8425d34be6dd6bcfbc5a1c
mpoore <2269147+mpoore@users.noreply.github.com>
2021-03-29 21:07:36 +0100

    Changed outfile

--- script/windows/84-wallpaper.ps1
+++ script/windows/84-wallpaper.ps1
@@ -8,4 +8,4 @@ $uri = ("REPLACEWITHINTRANET" + "/other/bginfo")
 $targetFolder = "C:\Windows\Web\Wallpaper\Windows"
 
 # Get files
-Invoke-WebRequest -Uri ($uri + "/v12n-desktop-background.jpg") -OutFile $targetFolder\img0.jpg
+Invoke-WebRequest -Uri ($uri + "/v12n-desktop-background.jpg") -OutFile $targetFolder\v12n-desktop-background.jpg
