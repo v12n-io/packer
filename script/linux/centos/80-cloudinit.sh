68d2cd10f5c78333fe68d537bd04d178672c2dfd
mpoore <2269147+mpoore@users.noreply.github.com>
2021-05-03 19:30:09 +0100

    Forogt -y

--- script/linux/centos/80-cloudinit.sh
+++ script/linux/centos/80-cloudinit.sh
@@ -60,4 +60,4 @@ chmod +rx /etc/cloud/runonce.sh
 echo "$(echo '@reboot ( sleep 30 ; sh /etc/cloud/runonce.sh )' ; crontab -l)" | crontab -
 
 # Add VMwareGuestInfo data source
-yum install https://github.com/vmware/cloud-init-vmware-guestinfo/releases/download/v1.1.0/cloud-init-vmware-guestinfo-1.1.0-1.el7.noarch.rpm
+yum install -y https://github.com/vmware/cloud-init-vmware-guestinfo/releases/download/v1.1.0/cloud-init-vmware-guestinfo-1.1.0-1.el7.noarch.rpm
