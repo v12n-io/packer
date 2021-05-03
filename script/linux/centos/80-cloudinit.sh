03f39951460154ede842228c8b56659f99890d2a
mpoore <2269147+mpoore@users.noreply.github.com>
2021-05-03 20:26:22 +0100

    Trying install.sh

--- script/linux/centos/80-cloudinit.sh
+++ script/linux/centos/80-cloudinit.sh
@@ -60,4 +60,5 @@ chmod +rx /etc/cloud/runonce.sh
 echo "$(echo '@reboot ( sleep 30 ; sh /etc/cloud/runonce.sh )' ; crontab -l)" | crontab -
 
 # Add VMwareGuestInfo data source
-yum install -y https://github.com/vmware/cloud-init-vmware-guestinfo/releases/download/v1.1.0/cloud-init-vmware-guestinfo-1.1.0-1.el7.noarch.rpm
+#yum install -y https://github.com/vmware/cloud-init-vmware-guestinfo/releases/download/v1.1.0/cloud-init-vmware-guestinfo-1.1.0-1.el7.noarch.rpm
+curl -sSL https://raw.githubusercontent.com/vmware/cloud-init-vmware-guestinfo/master/install.sh | sh -
