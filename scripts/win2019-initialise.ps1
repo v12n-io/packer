0de3b276c3dbf941bb4f5e4c8c19a0b097bb7f5f
mpoore <2269147+mpoore@users.noreply.github.com>
2021-09-01 13:30:32 +0100

    Disable IPv6

--- scripts/win2019-initialise.ps1
+++ scripts/win2019-initialise.ps1
@@ -21,6 +21,9 @@ winrm quickconfig -quiet
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
 
+# Disable IPV6
+Disable-NetAdapterBinding -Name "*" -ComponentID ms_tcpip6
+
 # Reset auto logon count
 # https://docs.microsoft.com/en-us/windows-hardware/customize/desktop/unattend/microsoft-windows-shell-setup-autologon-logoncount#logoncount-known-issue
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name AutoLogonCount -Value 0
