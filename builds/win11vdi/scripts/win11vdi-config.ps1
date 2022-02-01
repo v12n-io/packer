c0774da10e07a4db6eeef79d1bf28c8dc90b3067
mpoore <2269147+mpoore@users.noreply.github.com>
2022-02-01 09:47:55 +0000

    re-enable BGinfo

--- builds/win11vdi/scripts/win11vdi-config.ps1
+++ builds/win11vdi/scripts/win11vdi-config.ps1
@@ -73,21 +73,21 @@ $targetFolder = "C:\Windows\Web\v12n"
 Invoke-WebRequest -Uri ($uri + "/" + $sourcefile) -OutFile ($targetFolder + "\" + $sourcefile)
 
 # Install / Configure Bginfo
-#Write-Host " - Installing BGinfo ..."
-#$uri = ("REPLACEWITHINTRANET" + "/other/bginfo")
-#$targetFolder = "C:\Program Files\Bginfo"
-#New-Item $targetFolder -Itemtype Directory | Out-Null
-#Invoke-WebRequest -Uri ($uri + "/Bginfo.exe") -OutFile $targetFolder\Bginfo.exe
-#Invoke-WebRequest -Uri ($uri + "/v12n.bgi") -OutFile $targetFolder\v12n.bgi
-#$targetFile          = "$targetFolder\BGinfo.exe"
-#$shortcutFile        = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\Bginfo.lnk"
-#$scriptShell         = New-Object -ComObject WScript.Shell -Verbose
-#$shortcut            = $scriptShell.CreateShortcut($shortcutFile)
-#$shortcut.TargetPath = $targetFile
-#$arg1                = """$targetFolder\v12n.bgi"""
-#$arg2                = "/timer:0 /accepteula"
-#$shortcut.Arguments  = $arg1 + " " + $arg2
-#$shortcut.Save() | Out-Null
+Write-Host " - Installing BGinfo ..."
+$uri = ("REPLACEWITHINTRANET" + "/other/bginfo")
+$targetFolder = "C:\Program Files\Bginfo"
+New-Item $targetFolder -Itemtype Directory | Out-Null
+Invoke-WebRequest -Uri ($uri + "/Bginfo.exe") -OutFile $targetFolder\Bginfo.exe
+Invoke-WebRequest -Uri ($uri + "/v12n.bgi") -OutFile $targetFolder\v12n.bgi
+$targetFile          = "$targetFolder\BGinfo.exe"
+$shortcutFile        = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\Bginfo.lnk"
+$scriptShell         = New-Object -ComObject WScript.Shell -Verbose
+$shortcut            = $scriptShell.CreateShortcut($shortcutFile)
+$shortcut.TargetPath = $targetFile
+$arg1                = """$targetFolder\v12n.bgi"""
+$arg2                = "/timer:0 /accepteula"
+$shortcut.Arguments  = $arg1 + " " + $arg2
+$shortcut.Save() | Out-Null
 
 # Install Horizon Agent
 Write-Host " - Installing Horizon Agent ..."
