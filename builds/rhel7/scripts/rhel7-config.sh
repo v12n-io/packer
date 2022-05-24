a13248362537b894ec7d6ef36b5b35f8d28d4d69
Tô Thành Công <tcvn1985@gmail.com>
2022-05-24 15:16:23 +0700

    Update rhel7-config.sh

--- builds/rhel7/scripts/rhel7-config.sh
+++ builds/rhel7/scripts/rhel7-config.sh
@@ -157,8 +157,25 @@ if [ -f /var/log/lastlog ]; then
     echo '' | sudo tee /var/log/lastlog >/dev/null
 fi
 
-sudo echo "growpart /dev/sda 3 > /dev/null 2>&1" >> /etc/rc.local
-sudo echo "pvresize /dev/sda3 > /dev/null 2>&1" >> /etc/rc.local
-sudo echo "lvextend -l +100%FREE -r /dev/mapper/sysvg-lvroot > resize.txt 2>&1" >> /etc/rc.local
+sudo tee /etc/rc.local >/dev/null << ISSUE
+#!/bin/bash
+# THIS FILE IS ADDED FOR COMPATIBILITY PURPOSES
+#
+# It is highly advisable to create own systemd services or udev rules
+# to run scripts during boot instead of using this file.
+#
+# In contrast to previous versions due to parallel execution during boot
+# this script will NOT be run after all other services.
+#
+# Please note that you must run 'chmod +x /etc/rc.d/rc.local' to ensure
+# that this script will be executed during boot.
+
+touch /var/lock/subsys/local
+
+
+growpart /dev/sda 3 > /dev/null 2>&1
+pvresize /dev/sda3 > /dev/null 2>&1
+lvextend -l +100%FREE -r /dev/mapper/sysvg-lvroot > resize.txt
+ISSUE
 
 sudo echo ' - Configuration complete'
