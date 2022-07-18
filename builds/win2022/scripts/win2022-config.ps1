4a8b82d6ddb024d75747c4a0230e9c60a4e6aa2b
Michael Poore <michael@michaelpoore.com>
2022-07-18 11:07:06 +0100

    Script update

--- builds/win2022/scripts/win2022-config.ps1
+++ builds/win2022/scripts/win2022-config.ps1
@@ -3,13 +3,6 @@
 # @website https://blog.v12n.io
 $ErrorActionPreference = "Stop"
 
-# SettingSet Explorer view options
-Write-Host " - Setting default Explorer view options ..."
-Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "Hidden" 1 | Out-Null
-Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "HideFileExt" 0 | Out-Null
-Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "HideDrivesWithNoMedia" 0 | Out-Null
-Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "ShowSyncProviderNotifications" 0 | Out-Null
-
 # Disable system hibernation
 Write-Host " - Disabling system hibernation ..."
 Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Power\" -Name "HiberFileSizePercent" -Value 0 | Out-Null
