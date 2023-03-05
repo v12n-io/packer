788973410583c0d6d76cd4d4a888161c2115dd8f
Michael Poore <michael@michaelpoore.com>
2023-03-05 12:32:19 +0100

    Testing

--- vsphere/ubuntu2204/scripts/config.sh
+++ vsphere/ubuntu2204/scripts/config.sh
@@ -4,9 +4,9 @@
 # @website https://blog.v12n.io
 
 ## Disable IPv6
-#echo 'Disabling IPv6 in grub ...'
-#sudo sed -i 's|^\(GRUB_CMDLINE_LINUX.*\)"$|\1 ipv6.disable=1"|' /etc/default/grub
-#sudo grub2-mkconfig -o /boot/efi/EFI/redhat/grub.cfg &>/dev/null
+echo 'Disabling IPv6 in grub ...'
+sudo sed -i 's|^\(GRUB_CMDLINE_LINUX.*\)"$|\1 ipv6.disable=1"|' /etc/default/grub
+sudo update-grub &>/dev/null
 
 ## Apply updates
 echo 'Applying package updates ...'
