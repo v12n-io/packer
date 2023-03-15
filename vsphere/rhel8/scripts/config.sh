95ef9a61f1facca5aacdac448e5489d0ab9429a7
Michael Poore <michael@michaelpoore.com>
2023-03-15 18:56:13 +0000

    Testing script change

--- vsphere/rhel8/scripts/config.sh
+++ vsphere/rhel8/scripts/config.sh
@@ -119,9 +119,9 @@ sudo subscription-manager clean &>/dev/null
 
 ## Final cleanup actions
 echo 'Executing final cleanup tasks ...'
-if [ -f /etc/udev/rules.d/70-persistent-net.rules ]; then
-    sudo rm -f /etc/udev/rules.d/70-persistent-net.rules
-fi
+#if [ -f /etc/udev/rules.d/70-persistent-net.rules ]; then
+#    sudo rm -f /etc/udev/rules.d/70-persistent-net.rules
+#fi
 sudo rm -rf /tmp/*
 sudo rm -rf /var/tmp/*
 sudo rm -f /etc/machine-id
