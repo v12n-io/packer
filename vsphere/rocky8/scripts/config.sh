9c61ccd02b551ab8149106363a89f12d0c022bc9
Michael Poore <michael@michaelpoore.com>
2023-05-25 15:19:09 +0100

    Revisit cloud-init for Rocky 9

--- vsphere/rocky8/scripts/config.sh
+++ vsphere/rocky8/scripts/config.sh
@@ -55,36 +55,43 @@ done
 sudo update-ca-trust extract
 
 ## Configure cloud-init
-echo '-- Installing cloud-init ...'
-sudo touch /etc/cloud/cloud-init.disabled
-sudo sed -i 's/^ssh_pwauth:   0/ssh_pwauth:   1/g' /etc/cloud/cloud.cfg
-sudo sed -i -e 1,3d /etc/cloud/cloud.cfg
-sudo sed -i "s/^disable_vmware_customization: false/disable_vmware_customization: true/" /etc/cloud/cloud.cfg
-sudo sed -i "/disable_vmware_customization: true/a\\\nnetwork:\n  config: disabled" /etc/cloud/cloud.cfg
-sudo sed -i "s@^[a-z] /tmp @# &@" /usr/lib/tmpfiles.d/tmp.conf
-sudo sed -i "/^After=vgauthd.service/a After=dbus.service" /usr/lib/systemd/system/vmtoolsd.service
-sudo sed -i '/^disable_vmware_customization: true/a\datasource_list: [OVF]' /etc/cloud/cloud.cfg
-sudo cat << RUNONCE > /etc/cloud/runonce.sh
-#!/bin/bash
+echo '-- Configuring cloud-init ...'
+sudo cat << CLOUDCFG > /etc/cloud/cloud.cfg.d/99-vmware-guest-customization.cfg
+disable_vmware_customization: false
+datasource:
+  VMware:
+    vmware_cust_file_max_wait: 20
+CLOUDCFG
+
+#sudo touch /etc/cloud/cloud-init.disabled
+#sudo sed -i 's/^ssh_pwauth:   0/ssh_pwauth:   1/g' /etc/cloud/cloud.cfg
+#sudo sed -i -e 1,3d /etc/cloud/cloud.cfg
+#sudo sed -i "s/^disable_vmware_customization: false/disable_vmware_customization: true/" /etc/cloud/cloud.cfg
+#sudo sed -i "/disable_vmware_customization: true/a\\\nnetwork:\n  config: disabled" /etc/cloud/cloud.cfg
+#sudo sed -i "s@^[a-z] /tmp @# &@" /usr/lib/tmpfiles.d/tmp.conf
+#sudo sed -i "/^After=vgauthd.service/a After=dbus.service" /usr/lib/systemd/system/vmtoolsd.service
+#sudo sed -i '/^disable_vmware_customization: true/a\datasource_list: [OVF]' /etc/cloud/cloud.cfg
+#sudo cat << RUNONCE > /etc/cloud/runonce.sh
+##!/bin/bash
 # Runonce script for cloud-init on vSphere
 # @author Michael Poore
 # @website https://blog.v12n.io
 
 ## Enable cloud-init
-sudo rm -f /etc/cloud/cloud-init.disabled
+#sudo rm -f /etc/cloud/cloud-init.disabled
 ## Execute cloud-init
-sudo cloud-init init
-sleep 20
-sudo cloud-init modules --mode config
-sleep 20
-sudo cloud-init modules --mode final
+#sudo cloud-init init
+#sleep 20
+#sudo cloud-init modules --mode config
+#sleep 20
+#sudo cloud-init modules --mode final
 ## Mark cloud-init as complete
-sudo touch /etc/cloud/cloud-init.disabled
-sudo touch /tmp/cloud-init.complete
-sudo crontab -r
-RUNONCE
-sudo chmod +rx /etc/cloud/runonce.sh
-echo "$(echo '@reboot ( sleep 30 ; sh /etc/cloud/runonce.sh )' ; crontab -l)" | sudo crontab -
+#sudo touch /etc/cloud/cloud-init.disabled
+#sudo touch /tmp/cloud-init.complete
+#sudo crontab -r
+#RUNONCE
+#sudo chmod +rx /etc/cloud/runonce.sh
+#echo "$(echo '@reboot ( sleep 30 ; sh /etc/cloud/runonce.sh )' ; crontab -l)" | sudo crontab -
 
 ## Setup MoTD
 echo '-- Setting login banner ...'
